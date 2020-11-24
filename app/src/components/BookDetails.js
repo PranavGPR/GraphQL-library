1db97a12fe643b9edd1ca38b9bb62742aea5592a
PranavGPR <pranav.gnanasekar23@gmail.com>
2020-11-24 19:15:28 +0530

    book details display completed

--- app/src/components/BookDetails.js
+++ app/src/components/BookDetails.js
@@ -1,11 +1,36 @@
 import { graphql } from "react-apollo";
+import { getBookQuery } from "../queries/queries";
 
-function BookDetails() {
-  return (
-    <div id='book-details'>
-      <p>Output book details here</p>
-    </div>
-  );
+function BookDetails(props) {
+  const displayBookDetails = () => {
+    const { book } = props.data;
+    if (!!book) {
+      return (
+        <div>
+          <h2>{book.name}</h2>
+          <p>{book.genre}</p>
+          <p>{book.authorId}</p>
+          <p>All books by this author</p>
+          <ul className='other-books'>
+            {book.author.books.map((item) => {
+              return <li key={item.id}>{item.name}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    } else {
+      return <div>No book selected...</div>;
+    }
+  };
+  return <div id='book-details'>{displayBookDetails()}</div>;
 }
 
-export default BookDetails;
+export default graphql(getBookQuery, {
+  options: (props) => {
+    return {
+      variables: {
+        id: props.bookid,
+      },
+    };
+  },
+})(BookDetails);
--- app/src/components/BookList.js
+++ app/src/components/BookList.js
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import { graphql } from "react-apollo";
 
 import { getBooksQuery } from "../queries/queries";
+import BookDetails from "./BookDetails";
 
 function BookList(props) {
+  const [selected, setSelected] = useState("");
+
   const displayBooks = () => {
     let data = props.data;
     if (data.loading) {
       return <div>Loading Books...</div>;
     } else {
       return data.books.map((book) => {
-        return <li key={book.id}>{book.name}</li>;
+        return (
+          <li
+            key={book.id}
+            onClick={(e) => {
+              setSelected(book.id);
+            }}
+          >
+            {book.name}
+          </li>
+        );
       });
     }
   };
@@ -17,6 +30,7 @@ function BookList(props) {
   return (
     <div>
       <ul id='book-list'>{displayBooks()}</ul>
+      <BookDetails bookid={selected} />
     </div>
   );
 }
